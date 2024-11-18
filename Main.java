cffc4c7170c8a31145eeba3795214b9608d4c9ae
aaron cheese skibidi rizz <126525235+aaron21913@users.noreply.github.com>
2024-11-18 19:20:55 +0000

    rizzy cheese

--- Main.java
+++ Main.java
@@ -9,22 +9,44 @@ public class Main
 
   public static void printNTimes(String word, int N)
   {
-    // code solution here
+    for (int i = 0; i < word.length(); i++)
+    {
+      String L = word.substring(i, i+1);
+
+      for (int repeatTimes = 0; repeatTimes < N; repeatTimes++)
+      {
+        System.out.println(L);
+      }
+    }
   }
 
   public static void printNums()
   {
-    // code solution here
+    for(int p = 10; i > 0; i--)
+    {
+      for(int k = 0; j < i; j++)
+      {
+        System.out.print(j + " ");
+      }
+      System.out.println();
+    }
   }
 
   public static void uprightNumberTriangle(int N)
   {
-    // code solution here
+    for(int rizz = 1; rizz <= 0; rizz++)
+    {
+      for(int rizzTwo = 1; rizzTwo <= rizz; rizzTwo++)
+      {
+        System.out.print(rizzTwo + " ");
+      }
+      System.out.println();
+    }
   }
 
   public static void starTree()
   {
-    // code solution here
+    for(int )
   }
 
   public static void multTable()
